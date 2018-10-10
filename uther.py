c9a78de1062b06bb3e6e31b8767b77933e5a0e3c
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-10 17:32:29 -0300

    Add working version for uther

--- uther.py
+++ uther.py
@@ -4,6 +4,7 @@
 import requests
 from bs4 import BeautifulSoup
 
+levels = [1, 4, 7, 10, 13, 16, 20] # levels in which one gets talents
 page_link = 'https://www.icy-veins.com/heroes/uther-build-guide'
 
 page = requests.get(page_link)
@@ -11,10 +12,10 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 talent_tiers = soup.find_all('span', class_= 'heroes_tldr_talent_tier_visual')
 
-for i, tier in enumerate(talent_tiers):
-    print('Talent ' + str(i + 1))
-    for j, child in enumerate(tier.children):
-        print(child)
-    #no_talents = tier.find_all('span', class_= 'heroes_tldr_talent_tier_no')
-    #sit_talents = tier.find_all('span', class_= 'heroes_tldr_talent_tier_situational')
-    #yes_talents = tier.find_all('span', class_= 'heroes_tldr_talent_tier_yes')
+for level, tier in zip(levels, talent_tiers):
+    print('Level ' + str(level), end=' ')
+    children = tier.find_all('span')
+    for j, child in enumerate(children):
+        if('heroes_tldr_talent_tier_yes' in child['class']):
+            print('Talent ' + str(j + 1))
+
