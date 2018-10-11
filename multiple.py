228a8042c43aae48369e9004e78a41c7fe62e3ba
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-11 10:28:25 -0300

    Fix titles for all heroes

--- multiple.py
+++ multiple.py
@@ -21,8 +21,14 @@ def get_builds(hero):
 
     builds_tags = soup.find_all('div', class_='heroes_tldr_talents')
     build_title_tags = soup.find_all('h4', class_='toc_no_parsing')
-    build_titles = [title.get_text()[:-(len(' (talent calculator link)'))] for title in build_title_tags]
-    build_titles = [title[:-1] if title[-1] is '\n' else title for title in build_titles]
+
+    # strip ' (talent calculator link)' from title
+    strip_size = (len(' (talent calculator link)') + 1)
+    build_titles = [title.get_text()[:-strip_size] for title in build_title_tags]
+
+    # sometimes there is a '\n' we need to strip, if last letter is a 'd'
+    # then we striped the '\n', else we striped a 'd' we need to put back
+    build_titles = [title + 'd' if len(title) >= 1 and title[-1] is not 'd' else title for title in build_titles]
 
     for build_number, build_tag in enumerate(builds_tags):
         build = []
