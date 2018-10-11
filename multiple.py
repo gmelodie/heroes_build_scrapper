60097fa2449082bf0a582f39a710c221731e584e
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-11 09:28:08 -0300

    Fix build title string

--- multiple.py
+++ multiple.py
@@ -5,14 +5,14 @@ import requests
 from bs4 import BeautifulSoup
 
 levels = [1, 4, 7, 10, 13, 16, 20] # levels in which one gets talents
-page_link = 'https://www.icy-veins.com/heroes/alexstrasza-build-guide'
+page_link = 'https://www.icy-veins.com/heroes/varian-build-guide'
 
 page = requests.get(page_link)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 builds = soup.find_all('div', class_='heroes_tldr_talents')
 build_title_tags = soup.find_all('h4', class_='toc_no_parsing')
-build_titles = [title.get_text() for title in build_title_tags]
+build_titles = [title.get_text()[:-(len(' (talent calculator link)')+1)] for title in build_title_tags]
 
 for build_number, build in enumerate(builds):
     print('============== ' + str(build_titles[build_number]) + ' ==============')
