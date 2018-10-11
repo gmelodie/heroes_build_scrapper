b6095f814f17f24c191af0d24b95d841dc28e440
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-11 18:17:19 -0300

    Refactor scrapping code

--- scrapping.py
+++ scrapping.py
@@ -1,11 +1,30 @@
 
 # Scrapes alextraza's build (has 2 builds)
 
-from utils import print_build, normalize_hero_name
+from utils import print_build, normalize_hero_name, get_soup
 import requests
 from bs4 import BeautifulSoup
 
 
+def get_builds_titles(build_title_tags):
+    build_titles = []
+
+    # remove (strip) ' (talent calculator link)' from title
+    strip_size = (len(' (talent calculator link)') + 1)
+    build_titles = [title.get_text()[:-strip_size] for title in build_title_tags]
+
+    # sometimes there is a '\n' we need to strip, if last letter is a 'd'
+    # then we striped the '\n', else we striped a 'd' we need to put back
+    for title in build_titles:
+        if len(title) >= 1 and title[-1] is not 'd':
+            build_titles.append(title + 'd')
+        else:
+            build_titles.append(title)
+
+    # remove empty titles
+    build_titles = [title for title in build_titles if title != '']
+
+    return build_titles
 
 '''
 Scrape icy-veins for builds for the given hero
@@ -14,38 +33,27 @@ and the second one with the builds' titles
 '''
 def get_hero_builds(hero):
     builds = []
-
     hero = normalize_hero_name(hero)
-
-
-    page_link = 'https://www.icy-veins.com/heroes/' + hero + '-build-guide'
-
-    page = requests.get(page_link)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    link = 'https://www.icy-veins.com/heroes/' + hero + '-build-guide'
+    soup = get_soup(link)
 
     builds_tags = soup.find_all('div', class_='heroes_tldr_talents')
     build_title_tags = soup.find_all('h4', class_='toc_no_parsing')
 
-    # strip ' (talent calculator link)' from title
-    strip_size = (len(' (talent calculator link)') + 1)
-    build_titles = [title.get_text()[:-strip_size] for title in build_title_tags]
-
-    # sometimes there is a '\n' we need to strip, if last letter is a 'd'
-    # then we striped the '\n', else we striped a 'd' we need to put back
-    build_titles = [title + 'd' if len(title) >= 1 and title[-1] is not 'd' else title for title in build_titles]
+    build_titles = get_builds_titles(build_title_tags)
 
     for build_number, build_tag in enumerate(builds_tags):
         build = []
         talent_tiers = build_tag.find_all('span', class_= 'heroes_tldr_talent_tier_visual')
 
+        # find out which block is painted with green (chosen talent)
         for tier in talent_tiers:
             children = tier.find_all('span')
             for j, child in enumerate(children):
                 if('heroes_tldr_talent_tier_yes' in child['class']):
                     build.append(j+1)
-        builds.append(build[:])
 
-    # remove empty titles
-    build_titles = [title for title in build_titles if title != '']
+        # append whole build to builds list
+        builds.append(build[:])
 
     return builds, build_titles
