ced0f7e81a1c866ccec3c3d59cbbf14e4a6b3030
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-11 18:32:56 -0300

    Fix normalize_hero_name bug

--- utils.py
+++ utils.py
@@ -20,10 +20,9 @@ def print_build(levels, build, title):
 Clears string so that fits link
 '''
 def normalize_hero_name(hero):
-    hero = str(unicodedata.normalize('NFD', hero).encode('ascii', 'ignore'))
+    hero = unicodedata.normalize('NFD', hero).encode('ascii', 'ignore')
+    hero = str(hero.decode('utf-8'))
     hero = hero.replace(' ', '')
-    hero = hero.replace("'", '')
-    hero = hero[1:]
+    hero = hero.replace('\'', '')
     return hero.lower()
-
 
