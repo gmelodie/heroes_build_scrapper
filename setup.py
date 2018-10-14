f7b9cdae236e67a2e5caaae40ac17f438937a0a8
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-14 11:41:24 -0300

    Add levels file to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 import os
+import json
 from setuptools import setup, find_packages
 
 folder = 'data/builds'
 if not os.path.exists(folder):
     os.makedirs(folder)
+
+# Create levels.json file
+if not os.path.exists('data/levels.json'):
+    levels = [1, 4, 7, 10, 13, 16, 20]
+    with open('data/levels.json', 'w') as fp:
+        json.dump(levels, fp)
 
 with open('requirements.txt') as fp:
     required = fp.read().splitlines()
