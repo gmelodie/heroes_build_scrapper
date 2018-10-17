f8614a7c2070a77b01fd8517c9bc889c7e7bcda3
Gabriel Cruz <gabs.oficial98@gmail.com>
2018-10-17 13:57:49 -0300

    Minor fixes

--- setup.py
+++ setup.py
@@ -1,27 +1,5 @@
-import os
-import json
 from setuptools import setup, find_packages
-from setuptools.command.install import install
 
-
-class PostInstallCommand(install):
-    '''Post-installation for installation mode.'''
-    def run(self):
-        folder = 'data/builds'
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-
-        # Create levels.json file
-        if not os.path.exists('data/levels.json'):
-            levels = [1, 4, 7, 10, 13, 16, 20]
-            with open('data/levels.json', 'w') as fp:
-                json.dump(levels, fp)
-
-        # Create heroes.json file
-        if not os.path.exists('data/heroes.json'):
-            update_heroes_list()
-
-        install.run(self)
 
 with open('requirements.txt') as fp:
     required = fp.read().splitlines()
@@ -35,7 +13,4 @@ setup(
     packages=find_packages('src'),
     package_dir={"": "src"},
     description='A library for scraping builds for Heroes of the Storm heroes.',
-    cmdclass={
-        'install': PostInstallCommand,
-    },
 )
--- src/heroes_build_scrapper/__init__.py
+++ src/heroes_build_scrapper/__init__.py
@@ -1,3 +1,20 @@
 from .data import update_heroes_list, update_all_builds, print_all_builds
+import os
+import json
 
 name = 'heroes build scrapper'
+
+# Create directories
+folder = 'data/builds'
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+# Create levels.json file
+if not os.path.exists('data/levels.json'):
+    levels = [1, 4, 7, 10, 13, 16, 20]
+    with open('data/levels.json', 'w') as fp:
+        json.dump(levels, fp)
+
+# Create heroes.json file
+if not os.path.exists('data/heroes.json'):
+    update_heroes_list()
